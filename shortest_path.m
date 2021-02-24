6bab886ce2931ba9da2aac8e6e746c415d19834e
mrslvs <sekerka.miroslav@outlook.com>
2021-02-24 20:52:09 +0100

    fixed fitness function

--- shortest_path.m
+++ shortest_path.m
@@ -5,25 +5,41 @@ start = [0,0];
 stop = [100, 100];
 set = [18,77; 12,88; 30,37; 41,64; 10,19; 72,37; 87,17; 35,82;
 18,15; 18,90; 23,57; 75,12; 97,27; 7,67; 20,82; 49,0; 62,14; 10,35];
-
 path = [start; set; stop];
 
+figure(1)
 plot(path(:,1),path(:,2),'rx');
+grid;
+
+pop_size = 100;
+population = [];
 
 for cycle=1:200
-    fit = fitness(path)
+    
+    %fill in population
+    for i=1:pop_size
+        population(i, :, :) = path;
+        set = shake(set, 1);
+        set = swapgen(set, 0.5);
+        path = [start; set; stop];
+    end
+    
+    for i=1:pop_size
+        fit(i)=fitness(population(i, :, :));
+    end
+    
+    best_individuals(cycle) = min(fit);
 end
 
-function digit = get_random_digit(smax, smin)
-%returns random digit belonging to interval <0,9>
-    digit = round(rand() * (smax - smin + 1) + smin);
-end
+figure(2)
+plot(best_individuals);
+grid;
 
 function fit = fitness(path)
     fit = 0;
     
-    for i=1:(length(path)-1)
-        fit = fit + length_A_to_B(path(i, :), path(i+1, :));
+    for i=1:19
+        fit = fit + length_A_to_B(path(1, i, :), path(1, i+1, :));
     end
 end
 
