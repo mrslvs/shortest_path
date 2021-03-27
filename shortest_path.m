d9b78e9dc5db81015a80864adda4ad1affffc1d0
mrslvs <sekerka.miroslav@outlook.com>
2021-03-27 17:22:28 +0100

    revised code using global variables

--- shortest_path.m
+++ shortest_path.m
@@ -2,15 +2,13 @@
 %isid92###
 global start;
 start = [0,0];
-
 global set;
 set = [18,77; 12,88; 30,37; 41,64; 10,19; 72,37; 87,17; 35,82;
 18,15; 18,90; 23,57; 75,12; 97,27; 7,67; 20,82; 49,0; 62,14; 10,35];
-
 global stop;
 stop = [100, 100];
 
-
+global pop_size;
 pop_size = 50;
 population = [];
 vec_of_best_ones = [3, 2 ,1];
@@ -19,16 +17,17 @@ cycles = 900;
 for i=1:pop_size
     %fill the population with numbers(1 to 18)
     %each number represents index in the initial set
+    %set(1) = [18,77]
     population = [population;1:18];
 end
 population = swapgen(population, 0.3); %swap for diversity
 
 for i=1:cycles
-    fit = fitness(population, pop_size, set);
+    fit = fitness(population);
     best_individuals(i) = min(fit);
     best_one = selbest(population, fit, 1);
     
-    p = get_path(best_one, set);
+    p = get_path(best_one);
     path = [start; p; stop];
     
     %show path of the best individual in current cycle
@@ -50,13 +49,17 @@ for i=1:cycles
     
 end
 
-fit = fitness(population, pop_size, set);
+fit = fitness(population);
 
 figure(2)
 plot(best_individuals);
 grid;
 
-function path = get_path(order, set)
+function path = get_path(order)
+%take order(indexes) of points stored in set
+%returns path of points [x1,y1 ; x2,y2 ; ...]
+    global set;
+    
     for j=1:18
         %J: for each index of set
         for k=1:2
@@ -69,23 +72,22 @@ function path = get_path(order, set)
     end
 end
 
-function fit = fitness(population, pop_size, set)
+function fit = fitness(population)
+%fitness of individual is the length of its path
+    global pop_size;
+    
     for i=1:pop_size
-        %I: for each individual in population
-        path = get_path(population(i, :), set)
-        
-        %fitness of individual is the length of its path
+        path = get_path(population(i, :));
         fit(i) = length_of_path(path);
     end
 end
 
-function len = length_of_path(individ)
-%argument are indexes of initial set
-    %start = [0,0];
-    %stop = [100,100];
+function len = length_of_path(p)
+%argument are indexes of initial set [x1,y1 ; x2,y2 ; ...]
+%returns total length
     global start;
     global stop;
-    path = [start;individ;stop];
+    path = [start;p;stop];
     len = 0;
     
     for i=1:19
@@ -95,7 +97,7 @@ function len = length_of_path(individ)
 end
 
 function len = length_A_to_B(A, B)
-%argument A=[Xa,Yb]
+%argument A=[Xa,Ya]
 %returns distance between A&B
     diff_x = abs(A(1,1) - B(1,1));
     diff_y = abs(A(1,2) - B(1,2));
